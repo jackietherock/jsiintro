4555018ec2b811469560a5c1cfebf8f50cc15721
vadymkurylo <vadimkurilo13@gmail.com>
2025-06-24 16:56:33 +0300

    all tasks

---
+++ hello.js
@@ -0,0 +1,19 @@
+let name = prompt(`enter your name`);
+const greeting = 'Hello, ';
+alert(greeting + `${name}!`);
+
+let birthYear = +prompt(`enter your birth year`)
+const currentYear = 2025
+alert(currentYear - birthYear)
+
+let squareSideLength = +prompt('enter the square side length')
+const sidesAmount = 4
+alert(sidesAmount * squareSideLength)
+
+let distance = +prompt(`enter the distance in kilometers`)
+let timeOfArrival = +prompt(`enter time for ride in hours`)
+alert(distance  / timeOfArrival)
+
+const euroCurrency = 0.86
+let dollarToEuros = +prompt(`enter dollars amount`)
+alert(euroCurrency * dollarToEuros)
